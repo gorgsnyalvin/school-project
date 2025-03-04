b8866dc143f13697f23f3b1dce962da43070e45f
gorgsnyalvin <gorgsnyalvin@gmx.com>
2025-03-04 15:42:12 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Random {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        System.out.println(rand.nextInt(10));
+    }
+}
